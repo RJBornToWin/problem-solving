ecaee230eb08fccb14fd1357cb6ec55d914e94e0
RJBornToWin <rajasri215@gmail.com>
2022-10-08 21:55:01 -0700

    Bank robbery solution

--- Java/hc-problem-solving-practise/src/problemsolving/week/one/BankRobberySolution.java
+++ Java/hc-problem-solving-practise/src/problemsolving/week/one/BankRobberySolution.java
@@ -7,7 +7,7 @@ public class BankRobberySolution {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		BankRobbery br = new BankRobbery();
-		int[] coins = {9, 4, 6, 6, 1};
+		int[] coins = {21, 4, 6, 6, 1};
 				// {2, 4, 3, 6, 1};
 				// {2, 4, 3, 2, 1}; 
 				// {2, 4, 7, 7, 1};
@@ -27,34 +27,39 @@ class BankRobbery {
 	private int calcMax(int N, int[] banks) {
 		
 		int max = 0;
-		//Stack<Integer> st = new Stack<Integer>();
+		Stack<Integer> st = new Stack<Integer>();
 		
 		for (int i=N-1; i >= 0; i--) {
 			
 			int commonFactor = banks[i];
 			max =  Math.max(commonFactor, max);
-			
+			st.push(commonFactor);
+			System.out.println(st.peek());	
 			
 			int localIndex = i -1;
 			
 			while (localIndex >= 0 && banks[localIndex] > 0) {
 				if (banks[localIndex] >= commonFactor) {
+					st.push(banks[localIndex]);
+					System.out.println(st.peek());
 					int gold = commonFactor * (i-localIndex+1);
-					//st.push(gold);
-					//System.out.println(st.peek());	
+					st.push(gold);
+					System.out.println(st.peek());	
 					max = Math.max(gold, max);
 				}
 				else {
 						
 					commonFactor = banks[localIndex];
 					int gold = commonFactor * (i-localIndex+1);
-					//System.out.println("Gold is: " + gold);
-					//st.push(gold);
+					st.push(gold);
+					System.out.println(st.peek());	
 					max = Math.max(gold, max);
 				}
 				localIndex--;
 
 			}
+			System.out.println("--------");	
+			
 		}
 		
 		//while (!st.isEmpty()) {
